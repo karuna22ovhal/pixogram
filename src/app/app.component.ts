fd3c006978568a35449f5f2db12731c913c347b5
karuna22ovhal <51225990+karuna22ovhal@users.noreply.github.com>
2019-08-08 15:49:21 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { single } from 'rxjs/operators';
-
-import { NewsApiService } from './news-headlines/news-api.service';
-
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,66 +6,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NewsApp';
-  render: boolean;
-  ren = true;
-  render1= false;
-  ren1 = true;
-
-  name
-  password
-
-
-
-
-
-  saveEmployee(empform: NgForm) {
-    console.log("Hello")
-    this.name = empform.value.Name
-    console.log(this.name)
-    this.password = empform.value.Password
-    console.log(this.password)
-
-    this.render1 = true;
-
-    this.ren1 = false;
-    this.ren = false;
-    this.render = false;
-
-  }
-
-
-  saveEmployee1(empform: NgForm) {
-    console.log("Hello")
-    
-
-    this.render = true;
-
-    this.ren = false;
-
-    this.ren1 = false;
-    this.render1 = false;
-  }
-
-
-  mArticles: Array<any>;
-  mSources: Array<any>;
-
-  constructor(private newsapi: NewsApiService) {
-    console.log('app component constructor called');
-  }
-
-  ngOnInit() {
-    //load articles
- //   this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
-    //load news sources
-   // this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
-  }
-
-  searchArticles(source) {
-    //console.log("selected source is: " + source);
-    //this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
-  }
-
+  title = 'signup';
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { NewsApiService } from './news-headlines/news-api.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
-import { SignupComponent } from './signup/signup.component';
-import { NgForm, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NewsHeadlinesComponent } from './news-headlines/news-headlines.component';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule,MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { AdminComponentComponent } from './admin-component/admin-component.component';
+import { MyMaterialModule } from './material.module';
+import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NewsComponentComponent } from './news-component/news-component.component';
+import { NewsApiService } from './news-component/news-api.service';
+import { MatCardModule, MatMenuModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
-   SignupComponent,
-   NewsHeadlinesComponent
+    RegistrationComponentComponent,
+    LoginComponentComponent,
+    AdminComponentComponent,
+    NewsComponentComponent
     
+   
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyMaterialModule,
+    MatButtonModule,
+    MatToolbarModule,
+    BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatButtonModule,
     MatMenuModule,
     MatCardModule,
+    HttpModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    AppRoutingModule,
-      FormsModule
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: 'register', component: RegistrationComponentComponent },
+      { path: 'login', component: LoginComponentComponent },
+      { path: 'admin', component: AdminComponentComponent },
+      { path: 'news', component: NewsComponentComponent },
+
+ 
+    ]),
   ],
   providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
-
-
-
-
-
